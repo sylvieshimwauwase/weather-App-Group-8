eb41d2905d0305394693c7fb9b2f17437e5adf53
ZeeAzeez <azeezahaazeez@gmail.com>
2023-12-13 14:14:49 +0100

    weather app

---
+++ script.js
@@ -0,0 +1,56 @@
+let weather = {
+    "apiKey": "8d4ce9534d0890f4fe7f8075f5257aa3",
+    fetchWeather: function (city) {
+      fetch(
+        "https://api.openweathermap.org/data/2.5/weather?q=" +
+          city +
+          "&appid=" +
+          this.apiKey
+      )
+        .then((response) => response.json())
+        .then((data) =>{
+            if (data.cod === "404") {
+       
+                this.displayError("Location doesn't exist");
+              } else {
+                this.displayWeather(data);
+              }
+        })
+        .catch((error) => {
+            console.error("Error fetching weather data:", error);
+            this.displayError("Error fetching weather data");
+          });
+    },
+    displayWeather: function (data) {
+      const { name } = data;
+      const { icon, description } = data.weather[0];
+   
+      const { temp, humidity } = data.main;
+      const { speed } = data.wind;
+   
+      document.querySelector(".city").innerHTML = "Weather in " + name;
+      document.querySelector(".icon").src =
+        "http://openweathermap.org/img/wn/" + icon + ".png";
+      
+        https: document.querySelector(".description").innerHTML = description;
+        document.querySelector(".temp").innerHTML = temp + "°C";
+        document.querySelector(".humidity").innerHTML = "Humidity: " + humidity + "%";
+        document.querySelector(".wind").innerHTML = "Wind Speed: " + speed + " km/h";
+    document.querySelector(".weather").classList.remove("loading");
+   
+    },
+    search: function(){
+      this.fetchWeather(document.querySelector("input").value);
+    }
+  };
+  document.querySelector("button").addEventListener("click", () => {
+  weather.search();
+  });
+  document.querySelector("input").addEventListener("keyup", (event) => {
+    if(event.key == "Enter"){
+      weather.search();
+    }
+   
+  });
+  weather.fetchWeather("Nigeria");
+   
